a198a607bbf1763f1fcaf04daf3b049118c7662a
DahMooingCow <cameronmcfie15@gmail.com>
2018-12-06 01:10:24 +1300

    Update

--- Orbit_Simv5.py
+++ Orbit_Simv5.py
@@ -30,7 +30,7 @@ shotSpeed = 15
 planetList, satList, missileList, shotList, asteroidList = [], [], [], [], []
 text = ' '
 numberOfPlanets = 0
-numberOfAsteroids = 100
+numberOfAsteroids = 500
 fired = 0  # Turns to 1 if shots have been fired
 frames, actualFps, count, degrees = 0, 0, 0, 0
 # Constants
@@ -68,8 +68,8 @@ class Player(pygame.sprite.Sprite):
         self.triangle = [0, (3 * math.pi / 4), (5 * math.pi / 4)]  # Points around a circle describing a triangle
         self.triangle = map(lambda x: x+(2*math.pi/4), self.triangle)  # Adding 90 degrees to angles
         for t in self.triangle:  # Makes these angles to x, y pos
-            self.x.append(self.xPos + self.radius * math.cos(t + -self.angle))  # Adds these coordinates to list
-            self.y.append(self.yPos + self.radius * math.sin(t + -self.angle))      # Adds these coordinates to list
+            self.x.append(self.xPos + self.radius * math.cos(t + -self.angle))   # Adds these coordinates to list
+            self.y.append(self.yPos + self.radius * math.sin(t + -self.angle))   # Where the magic happens
         self.triangle = [(self.x[0], self.y[0]), (self.x[1], self.y[1]), (self.x[2], self.y[2])]  # X, Y Pos List
         self.triangle = pygame.draw.polygon(screen, colourDict['white'], self.triangle, 2)  # Draws the ship
 
@@ -99,10 +99,13 @@ class Asteroids():
         self.yVel = random.uniform(-5, 5)
         self.xAcceleration, self.yAcceleration, self.angle = 0, 0, 0
         self.rotationSpeed = random.uniform(0,1)
-        print(self.rotationSpeed, self.xPos, self.yPos)
-        self.radius = 20
-        self.poly, self.x, self.y = [], [], []
+        self.radius = random.randint(5,100)
+        self.polyList, self.xy, self.poly = [], [], []
+        self.x, self.y = 0, 0
         asteroidList.append(self)
+        for i in range(0,10):
+            self.point = random.uniform(0, math.tau)
+            self.polyList.append(self.point)
 
     def update(self):
         self.updatePoly()
@@ -111,14 +114,16 @@ class Asteroids():
         self.force = 0  # Resets force when button is not pushed down
 
     def updatePoly(self):
-        self.x, self.y = [], []
-        self.triangle = [0, (3 * math.pi / 4), (5 * math.pi / 4)]  # Points around a circle describing a triangle
-        self.triangle = map(lambda x: x+(2*math.pi/4), self.triangle)  # Adding 90 degrees to angles
-        for t in self.triangle:  # Makes these angles to x, y pos
-            self.x.append(self.xPos + self.radius * math.cos(t + -self.angle))  # Adds these coordinates to list
-            self.y.append(self.yPos + self.radius * math.sin(t + -self.angle))      # Adds these coordinates to list
-        self.triangle = [(self.x[0], self.y[0]), (self.x[1], self.y[1]), (self.x[2], self.y[2])]  # X, Y Pos List
-        self.triangle = pygame.draw.polygon(screen, colourDict['white'], self.triangle, 2)  # Draws the ship
+        #self.triangle = [0, (3 * math.pi / 4), (5 * math.pi / 4)]  # Points around a circle describing a triangle
+        #self.triangle = map(lambda x: x+(2*math.pi/4), self.triangle)  # Adding 90 degrees to angles
+        for pt in self.polyList:  # Makes these angles to x, y pos
+            self.x = self.xPos + self.radius * math.cos(pt)
+            self.y = self.yPos + self.radius * math.sin(pt)
+            self.xy = [self.x, self.y]  # Adds these coordinates to list
+            self.poly.append(self.xy)  # Adds all coordinates to master list
+        #self.poly = [(self.x[0], self.y[0]), (self.x[1], self.y[1]), (self.x[2], self.y[2])]  # X, Y Pos List
+        self.asteroid = pygame.draw.polygon(screen, colourDict['white'], self.poly, 2)  # Draws the ship
+        self.poly = []
 
     def posistionUpdate(self):
         self.xPos += self.xVel
@@ -126,7 +131,7 @@ class Asteroids():
 
     def boundCheck(self):
         if self.xPos < -1000 or self.xPos > (width+1000) or self.yPos < -1000 or self.yPos > (width+1000):
-            print("outa bounds!! howdy ho")
+            pass
 
 
 class Planet:
