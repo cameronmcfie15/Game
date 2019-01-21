5450c7d2bf4dd6054035f6dd8ab14239632cfa15
DahMooingCow <cameronmcfie15@gmail.com>
2019-01-22 00:42:05 +1300

    Pretty much finished

---
+++ dist/Orbit/Settings/settings.py
@@ -0,0 +1,12 @@
+# Default settings for Orbit (Change at your own risk)
+FRAMERATE = 60  # Changes Physics, change at your own risk
+DECAY = 0.98  # How quickly you slow down, like friction
+WINDOWWIDTH = 1280
+WINDOWHEIGHT = 960
+ASTEROIDSPAWN = 1  # Number of asteroids spawned per second
+ASTEROIDRATE = 10  # Bigger = Slower spawn rate, how fast the above value changes
+CASH = 0  # Starting Cash
+SHOTSPEED = 5  # Starting shot speed, increments of 5
+SHIELD = 0
+LIVES = 3
+
